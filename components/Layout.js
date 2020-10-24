c079b47fbe15a67de9ffb1b6f64c58071ecaa7b7
Nicolas Saporiti <nicolas.saporiti@gmail.com>
2020-10-23 22:51:56 -0300

    tipos de css

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,17 @@
 import React from "react";
-import Navbar from "../components/Navbar";
+import Navbar from "@components/Navbar";
 
 const Layout = ({ children }) => {
   return (
-    <div>
+    <div className="container">
       <Navbar />
       {children}
       <footer>Este es el footer</footer>
+      <style jsx>{`
+      .container {
+        background: salmon;
+      }
+      `}</style>
     </div>
   );
 };
